4dc1577019650002e083637be93493dd96c3db57
samaswin87 <aswin.r312@outlook.com>
2021-05-30 22:26:31 +0530

    added devise auth

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # issue: https://stackoverflow.com/questions/55626625/devise-token-auth-conflicts
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :lockable, :timeoutable, :trackable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+end
